1a35c9eef0a22fc6bb910d3d872e16e4d022db45
Slawomir Slowik <SlawomirSlowik@yahoo.pl>
2024-03-20 08:34:13 +0100

    templates tutorial part 3

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -1,16 +1,31 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = "<table><tr><th>Questions</th></tr>"
-    output += "".join(["<tr><td>" + q.question_text + "</td></tr>" for q in latest_question_list]) + "</table>"
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def counter(request):
     template = loader.get_template('counter.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+ return HttpResponse("You're voting on question %s." % question_id)
